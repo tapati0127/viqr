3b5dab81623f1de53fa46f28c3c2dfd7248da063
Phat Trien Thai <trientp@viettel.com.vn>
2025-09-20 21:17:14 +0700

    nomic-ai/nomic-embed-text-v1.5

--- create_index_dense.sh
+++ create_index_dense.sh
@@ -5,6 +5,17 @@ if [ ! -f "$OUTPUT" ]; then
     echo "Creating index..."
     python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
         encoder --encoder nomic-ai/nomic-embed-text-v1 --batch 16 --device cuda:0 --encoder-class sentence-transformers \
-        --dimension 1024 
+        --dimension 768 
+
+fi
+
+OUTPUT=./new_static/index/nomic-embed-text-v1.5
+INPUT=./new_static/doc_prompt
+
+if [ ! -f "$OUTPUT" ]; then
+    echo "Creating index..."
+    python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
+        encoder --encoder nomic-ai/nomic-embed-text-v1.5 --batch 16 --device cuda:0 --encoder-class sentence-transformers \
+        --dimension 768 
 
 fi
