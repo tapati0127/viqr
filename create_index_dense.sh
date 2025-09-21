4b408341334a8278865e37f41e0ecb6aeafbe1fd
Phat Trien Thai <trientp@viettel.com.vn>
2025-09-22 06:09:03 +0700

    run_index nowflake-arctic-embed-l-v2.0  gte-multilingual-base

--- create_index_dense.sh
+++ create_index_dense.sh
@@ -1,88 +1,68 @@
-export HF_HUB_ENABLE_HF_TRANSFER=1
-export TRANSFORMERS_TRUST_REMOTE_CODE=1
+# OUTPUT=./new_static/index/bge-m3
+# INPUT=./new_static/doc_prompt
+
+# if [ ! -f "$OUTPUT" ]; then
+#     echo "Creating index..."
+#     python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
+#         encoder --encoder BAAI/bge-m3 --batch 32 --device cuda:0 --encoder-class sentence-transformers \
+#         --dimension 1024 
+
+# fi
 
 
-OUTPUT=./new_static/index/bge-m3
-INPUT=./new_static/doc_prompt
+# OUTPUT=./new_static/index/nomic-embed-text-v1
+# INPUT=./new_static/doc_prompt
 
-if [ ! -f "$OUTPUT" ]; then
-    echo "Creating index..."
-    python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
-        encoder --encoder BAAI/bge-m3 --batch 32 --device cuda:0 --encoder-class sentence-transformers \
-        --dimension 1024 
+# if [ ! -f "$OUTPUT" ]; then
+#     echo "Creating index..."
+#     python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
+#         encoder --encoder nomic-ai/nomic-embed-text-v1 --batch 16 --device cuda:0 --encoder-class sentence-transformers \
+#         --dimension 768 
 
-fi
+# fi
+
+# OUTPUT=./new_static/index/nomic-embed-text-v1.5
+# INPUT=./new_static/doc_prompt
+
+# if [ ! -f "$OUTPUT" ]; then
+#     echo "Creating index..."
+#     python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
+#         encoder --encoder nomic-ai/nomic-embed-text-v1.5 --batch 16 --device cuda:0 --encoder-class sentence-transformers \
+#         --dimension 768 
+
+# fi
 
 
-INDEX_NAME=bge-m3
-INDEX_PATH=./new_static/index/${INDEX_NAME}/
-git lfs install
-git clone git@hf.co:datasets/trientp/${INDEX_NAME}
-cp -r ${INDEX_PATH} .
-cd ${INDEX_NAME}
-git add .
-git commit -m "Initial upload"
-git push
+# OUTPUT=./new_static/index/multilingual-e5-base
+# INPUT=./new_static/doc
 
-OUTPUT=./new_static/index/nomic-embed-text-v1
-INPUT=./new_static/doc_prompt
+# if [ ! -f "$OUTPUT" ]; then
+#     echo "Creating index..."
+#     python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
+#         encoder --encoder intfloat/multilingual-e5-base --batch 16 --device cuda:0 --encoder-class sentence-transformers \
+#         --dimension 768 
 
-if [ ! -f "$OUTPUT" ]; then
-    echo "Creating index..."
-    python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
-        encoder --encoder nomic-ai/nomic-embed-text-v1 --batch 16 --device cuda:0 --encoder-class sentence-transformers \
-        --dimension 768 
+# fi
 
-fi
-
-INDEX_NAME=nomic-embed-text-v1
-INDEX_PATH=./new_static/index/${INDEX_NAME}/
-git lfs install
-git clone git@hf.co:datasets/trientp/${INDEX_NAME}
-cd ${INDEX_NAME}
-cp -r ${INDEX_PATH} .
-git add .
-git commit -m "Initial upload"
-git push
-
-OUTPUT=./new_static/index/nomic-embed-text-v1.5
-INPUT=./new_static/doc_prompt
-
-if [ ! -f "$OUTPUT" ]; then
-    echo "Creating index..."
-    python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
-        encoder --encoder nomic-ai/nomic-embed-text-v1.5 --batch 16 --device cuda:0 --encoder-class sentence-transformers \
-        --dimension 768 
-
-fi
-
-INDEX_NAME=nomic-embed-text-v1.5
-INDEX_PATH=./new_static/index/${INDEX_NAME}/
-git lfs install
-git clone git@hf.co:datasets/trientp/${INDEX_NAME}
-cd ${INDEX_NAME}
-cp -r ${INDEX_PATH} .
-git add .
-git commit -m "Initial upload"
-git push
-
-OUTPUT=./new_static/index/multilingual-e5-base
+OUTPUT=./new_static/index/snowflake-arctic-embed-l-v2.0
 INPUT=./new_static/doc
 
 if [ ! -f "$OUTPUT" ]; then
     echo "Creating index..."
     python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
-        encoder --encoder intfloat/multilingual-e5-base --batch 16 --device cuda:0 --encoder-class sentence-transformers \
-        --dimension 768 
+        encoder --encoder Snowflake/snowflake-arctic-embed-l-v2.0 --batch 32 --device cuda:0 --encoder-class sentence-transformers \
+        --dimension 1024 
 
 fi
 
-INDEX_NAME=multilingual-e5-base
-INDEX_PATH=./new_static/index/${INDEX_NAME}/
-git lfs install
-git clone git@hf.co:datasets/trientp/${INDEX_NAME}
-cd ${INDEX_NAME}
-cp -r ${INDEX_PATH} .
-git add .
-git commit -m "Initial upload"
-git push
+
+OUTPUT=./new_static/index/gte-multilingual-base
+INPUT=./new_static/doc
+
+if [ ! -f "$OUTPUT" ]; then
+    echo "Creating index..."
+    python -m pyserini.encode input  --corpus ${INPUT} --shard-id 0 output  --embeddings ${OUTPUT} --to-faiss \
+        encoder --encoder Alibaba-NLP/gte-multilingual-base --batch 32 --device cuda:0 --encoder-class sentence-transformers \
+        --dimension 768 
+
+fi
